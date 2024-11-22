39e3f9520a8606324c3e84ddc29be37fc571f1e5
Pulini Tilanka <pulinitilanka@gmail.com>
2024-11-22 22:10:36 +0530

    Added Home Page

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Software Engineer", "Frontend Developer", "Backend Developer", "Fullstack Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
